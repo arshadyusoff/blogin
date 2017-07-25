d6865b78db68ea1c48bda41c0349dd5112bba7f8
Mohd Arshad Mohd Yusoff <arshad@Mohds-MacBook-Pro.local>
2017-07-25 22:28:42 +0800

    Add Bootstrap JS

--- functions.php
+++ functions.php
@@ -119,7 +119,7 @@ add_action( 'widgets_init', 'blogin_widgets_init' );
 function blogin_scripts() {
 	wp_enqueue_style( 'blogin-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'blogin-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'blogin-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
 
 	wp_enqueue_script( 'blogin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
