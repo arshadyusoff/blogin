c496c3fe2ee2e2752326a7b9bcfc692a578df2f2
Mohd Arshad Mohd Yusoff <arshad@Mohds-MacBook-Pro.local>
2017-07-26 13:56:45 +0800

    Clean up unnecessary files

--- functions.php
+++ functions.php
@@ -96,43 +96,14 @@ function blogin_content_width() {
 add_action( 'after_setup_theme', 'blogin_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ * Widgets
  */
-function blogin_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'blogin' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'blogin' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'blogin_widgets_init' );
+require get_template_directory() . '/inc/widgets.php';
 
 /**
- * Enqueue scripts and styles.
+ * Scripts
  */
-function blogin_scripts() {
-	wp_enqueue_style( 'blogin-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'blogin-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
-
-	wp_enqueue_script( 'blogin-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'blogin_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/scripts.php';
 
 /**
  * Custom template tags for this theme.
